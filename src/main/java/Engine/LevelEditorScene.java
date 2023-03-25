caf6e48c4e848435debf6fd50aafdecf775fa697
DotNaos <78213692+DotNaos@users.noreply.github.com>
2023-03-25 13:45:11 +0100

    SpriteSheets laden

--- src/main/java/Engine/LevelEditorScene.java
+++ src/main/java/Engine/LevelEditorScene.java
@@ -1,7 +1,9 @@
 package Engine;
 
 import Util.AssetPool;
+import components.Sprite;
 import components.SpriteRenderer;
+import components.Spritesheet;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 
@@ -13,23 +15,31 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
+        loadResources();
+
         // Camera center
         this.camera = new Camera(new Vector2f( -300, 0));
 
+        Spritesheet sprites = AssetPool.getSpritesheet("assets/images/Walk.png");
+
         // Create a game object
         GameObject obj1 = new GameObject("obj1", new Transform(new Vector2f(100, 256), new Vector2f(256, 256)));
-        obj1.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/Boden.png")));
+        obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
         GameObject obj2 = new GameObject("obj2", new Transform(new Vector2f(400, 256), new Vector2f(256, 256)));
-        obj2.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/character.png")));
+        obj2.addComponent(new SpriteRenderer(sprites.getSprite(5)));
         this.addGameObjectToScene(obj2);
 
-        loadResources();
+
     }
 
     private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
+
+        AssetPool.addSpritesheet("assets/images/Walk.png",
+                new Spritesheet(AssetPool.getTexture("assets/images/Walk.png"),
+                        117, 26, 8, 0));
 
     }
 
