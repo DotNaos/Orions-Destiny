5b4ad0ae192fc66b0cb2ceba53c7f2b317bbd034
DotNaos <schuetzoliver00@gmail.com>
2023-04-19 13:51:33 +0200

    Viewport coordinates

--- src/main/java/editor/GameViewWindow.java
+++ src/main/java/editor/GameViewWindow.java
@@ -3,9 +3,14 @@ package editor;
 import imgui.ImGui;
 import imgui.ImVec2;
 import imgui.flag.ImGuiWindowFlags;
+import Burst.MouseListener;
 import Burst.Window;
+import org.joml.Vector2f;
 
 public class GameViewWindow {
+
+    private static float leftX, rightX, topY, bottomY;
+
     public static void imgui() {
         ImGui.begin("Game Viewport", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse);
 
@@ -13,11 +18,30 @@ public class GameViewWindow {
         ImVec2 windowPos = getCenteredPositionForViewport(windowSize);
 
         ImGui.setCursorPos(windowPos.x, windowPos.y);
+
+        ImVec2 topLeft = new ImVec2();
+        ImGui.getCursorScreenPos(topLeft);
+        topLeft.x -= ImGui.getScrollX();
+        topLeft.y -= ImGui.getScrollY();
+        leftX = topLeft.x;
+        bottomY = topLeft.y;
+        rightX = topLeft.x + windowSize.x;
+        topY = topLeft.y + windowSize.y;
+
         int textureId = Window.getFramebuffer().getTextureID();
         ImGui.image(textureId, windowSize.x, windowSize.y, 0, 1, 1, 0);
 
+        MouseListener.setGameViewportPos(new Vector2f(topLeft.x, topLeft.y));
+        MouseListener.setGameViewportSize(new Vector2f(windowSize.x, windowSize.y));
+
         ImGui.end();
     }
+
+    public static boolean getWantCaptureMouse() {
+        return MouseListener.getX() >= leftX && MouseListener.getX() <= rightX &&
+                MouseListener.getY() >= bottomY && MouseListener.getY() <= topY;
+    }
+
     private static ImVec2 getLargestSizeForViewport() {
         ImVec2 windowSize = new ImVec2();
         ImGui.getContentRegionAvail(windowSize);
@@ -27,6 +51,7 @@ public class GameViewWindow {
         float aspectWidth = windowSize.x;
         float aspectHeight = aspectWidth / Window.getTargetAspectRatio();
         if (aspectHeight > windowSize.y) {
+            // We must switch to pillarbox mode
             aspectHeight = windowSize.y;
             aspectWidth = aspectHeight * Window.getTargetAspectRatio();
         }
@@ -43,11 +68,7 @@ public class GameViewWindow {
         float viewportX = (windowSize.x / 2.0f) - (aspectSize.x / 2.0f);
         float viewportY = (windowSize.y / 2.0f) - (aspectSize.y / 2.0f);
 
-
         return new ImVec2(viewportX + ImGui.getCursorPosX(),
-                viewportY + ImGui.getCursorPosY()
-        );
+                viewportY + ImGui.getCursorPosY());
     }
-
-
 }
