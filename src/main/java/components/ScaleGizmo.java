3255c0ada07e5f486e1d155825f453a4b414541a
DotNaos <78213692+DotNaos@users.noreply.github.com>
2023-04-25 10:23:41 +0200

    Switch to laptop

---
+++ src/main/java/components/ScaleGizmo.java
@@ -0,0 +1,11 @@
+package components;
+
+import editor.PropertiesWindow;
+
+public class ScaleGizmo extends Gizmo{
+
+    public ScaleGizmo(Sprite arrowSprite, PropertiesWindow propertiesWindow) {
+        super(arrowSprite, propertiesWindow);
+
+    }
+}
