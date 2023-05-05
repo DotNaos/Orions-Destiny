b450fcb5ea2ef240a02060396e01c30e491aa7cd
DotNaos <schuetzoliver00@gmail.com>
2023-05-05 13:17:50 +0200

    Menu Creator Klasse für ButtonGUIs

--- src/main/java/Burst/Engine/Source/Core/Scene/StartMenuSceneInitializer.java
+++ src/main/java/Burst/Engine/Source/Core/Scene/StartMenuSceneInitializer.java
@@ -1,34 +1,22 @@
 package Burst.Engine.Source.Core.Scene;
 
 import Burst.Engine.Source.Core.Graphics.Render.Texture;
+import Burst.Engine.Source.Core.UI.Menu;
 import Burst.Engine.Source.Core.UI.Window;
 import Orion.res.Assets;
 import imgui.ImGui;
 import imgui.flag.ImGuiCol;
 import imgui.flag.ImGuiStyleVar;
 import imgui.flag.ImGuiWindowFlags;
+import org.joml.Vector2f;
 
 
 public class StartMenuSceneInitializer extends SceneInitializer {
-
-        private float BUTTON_WIDTH = 400;
-        private float BUTTON_HEIGHT = 50;
-
+    
         // button colors
         private final int BUTTON_COLOR = 0xA0;
         private final int BUTTON_HOVER_COLOR = 0x80;
-
-        private float COLUMS = 2;
-        private float ROWS = 2;
-        private float buttonSpacing = 20;
-        private float currentX = 0;
-        private float currentY = 0;
-
-        private float ALIGNMENT_X = 100 / 50;
-        private float ALIGNMENT_Y = 100 / 50;
-
-        private float MARGIN_Y = 200;
-        private float MARGIN_X = 0;
+        
 
         @Override
         public void imgui()
@@ -56,37 +44,42 @@ public class StartMenuSceneInitializer extends SceneInitializer {
             // BOrdereinstellungen
             ImGui.pushStyleVar(ImGuiStyleVar.FrameBorderSize, 1);
 
+            float buttonSpacing = 20;
+            float currentX = 0;
+            float currentY = 0;
+
+            Vector2f alignment = new Vector2f(100 / 50, 100 / 50);
+            Vector2f buttonSize = new Vector2f(400, 100);
+
+            
+            Vector2f margin = new Vector2f(0, 200);
 
 
+//                        Texture texture = new Texture(512, 512);
+//            texture.init(Assets.TEXTURES + "testImage.png");
+//            ImGui.setCursorPosX((float) Window.getWidth() / 2);
+//            ImGui.imageButton(texture.getId(), 256, 256, 0, 1, 1, 0);
 
-            currentX = (Window.getWidth() - ((BUTTON_WIDTH * COLUMS) - (buttonSpacing * (COLUMS-1)))) / ALIGNMENT_X + MARGIN_X;
-            currentY = (Window.getHeight() - ((BUTTON_HEIGHT * ROWS) - (buttonSpacing * (ROWS-1)))) / ALIGNMENT_Y + MARGIN_Y;
-            ImGui.setCursorPosX(currentX);
-            ImGui.setCursorPosY(currentY);
-            if (ImGui.button("Editor", BUTTON_WIDTH, BUTTON_HEIGHT)) {
+
+            Menu buttonMenu = new Menu(2, 2, buttonSize, alignment, buttonSpacing, margin);
+            if (ImGui.button("Editor", buttonSize.x, buttonSize.y)) {
                 Window.changeScene(new LevelEditorSceneInitializer());
             }
 
 
-            ImGui.sameLine();
-            currentX -= -buttonSpacing - BUTTON_WIDTH;
-            ImGui.setCursorPosX(currentX);
-            if (ImGui.button("Play", BUTTON_WIDTH, BUTTON_HEIGHT)) {
+            buttonMenu.nextColumn();
+            if (ImGui.button("Play", buttonSize.x, buttonSize.y)) {
                 Window.changeScene(new LevelSceneInitializer());
             }
 
-            currentX = (Window.getWidth() - ((BUTTON_WIDTH * COLUMS) - (buttonSpacing * (COLUMS-1)))) / ALIGNMENT_X + MARGIN_X;
-            currentY += buttonSpacing + BUTTON_HEIGHT;
-            ImGui.setCursorPosX(currentX);
-            ImGui.setCursorPosY(currentY);
-            if (ImGui.button("Settings", BUTTON_WIDTH, BUTTON_HEIGHT)) {
+  
+            buttonMenu.nextRow();
+            if (ImGui.button("Settings", buttonSize.x, buttonSize.y)) {
                 Window.changeScene(new SettingsSceneInitializer());
             }
 
 
-//            Texture texture = new Texture(512, 512);
-//            texture.init(Assets.TEXTURES + "testImage.png");
-//            ImGui.imageButton(texture.getId(), 1024, 1024, 0, 1, 1, 0);
+
 
             ImGui.popStyleColor(3);
             ImGui.popStyleVar(2);
--- src/main/java/Burst/Engine/Source/Core/UI/Menu.java
+++ src/main/java/Burst/Engine/Source/Core/UI/Menu.java
@@ -6,56 +6,75 @@ import org.joml.Vector2f;
 import java.awt.*;
 
 public class Menu {
+    private float currentX;
+    private float currentY;
+    private Vector2f itemSize;
+    private int columns = 2;
+    private int currentColumn = 0;
 
-    private float buttonSpacing = 20;
+    private int rows = 2;
+    private int currentRow = 0;
+
+
+    private float spacing;
 
     private Vector2f alignment;
 
-    public Menu(Vector2f alignment)
+    private Vector2f margin;
+
+    public Menu(int columns, int rows, Vector2f itemSize)
     {
-        this.alignment = alignment;
+        new Menu(columns, rows, itemSize, new Vector2f(100 / 50, 100 / 50));
     }
 
-
-    public Menu()
+    public Menu(int columns, int rows, Vector2f itemSize, Vector2f alignment)
     {
-        this.alignment = new Vector2f((float) 100 / 50, (float) 100 / 50);
+        new Menu(columns, rows, itemSize, alignment, 20);
     }
 
-    public void ButtonMenu(int columns, int rows, float buttonWidth, float buttonHeight, float buttonSpacing, float marginX, float marginY)
+    public Menu(int columns, int rows, Vector2f itemSize, Vector2f alignment,float spacing)
     {
-        ButtonMenu(columns, rows, buttonWidth, buttonHeight, buttonSpacing, marginX, marginY, this.alignment);
+        new Menu(columns, rows, itemSize, alignment, spacing, new Vector2f(0));
+    }
+    public Menu(int columns, int rows, Vector2f itemSize, Vector2f alignment,float spacing, Vector2f margin)
+    {
+        this.columns = columns;
+        this.rows = rows;
+        this.spacing = spacing;
+        this.itemSize = new Vector2f(itemSize);
+        this.margin = new Vector2f(margin);
+        this.alignment = new Vector2f(alignment);
+
+        float spacingSumX = this.spacing * (this.columns-1);
+        float spacingSumY = this.spacing * (this.rows-1);
+        float itemSumX = this.itemSize.x * this.columns;
+        float itemSumY = this.itemSize.y * this.rows;
+
+        currentX = (Window.getWidth() - itemSumX - spacingSumX) / this.alignment.x + this.margin.x;
+        currentY = (Window.getHeight() - itemSumY - spacingSumY) / this.alignment.y + this.margin.y;
+
+        ImGui.setCursorPosY(currentY);
+        ImGui.setCursorPosX(currentX);
     }
 
-    public void ButtonMenu(int columns, int rows, float buttonWidth, float buttonHeight, float buttonSpacing)
+    public void nextRow()
     {
-        ButtonMenu(columns, rows, buttonWidth, buttonHeight, buttonSpacing, 0, 0, this.alignment);
+        currentColumn = 0;
+        currentRow++;
+
+        float spacingSumX = this.spacing * (this.columns-1);
+        float itemSumX = this.itemSize.x * this.columns;
+        currentY -= -itemSize.y - spacing;
+        currentX = (Window.getWidth() - itemSumX - spacingSumX) / this.alignment.x + this.margin.x;
+        ImGui.setCursorPosX(currentX);
+        ImGui.setCursorPosY(currentY);
     }
 
-    public void ButtonMenu(int columns, int rows, float buttonWidth, float buttonHeight)
+    public void nextColumn()
     {
-        ButtonMenu(columns, rows, buttonWidth, buttonHeight, buttonSpacing, 0, 0, this.alignment);
+        currentX -= -itemSize.x - spacing;
+        currentColumn++;
+        ImGui.sameLine();
+        ImGui.setCursorPosX(currentX);
     }
-    public void ButtonMenu(int columns, int rows, float buttonWidth, float buttonHeight, float buttonSpacing, float marginX, float marginY, Vector2f alignment)
-    {
-        float currentX = (Window.getWidth() - ((buttonWidth * columns) - (buttonSpacing * (columns-1)))) / alignment.x + marginX;
-        float currentY = (Window.getHeight() - ((buttonHeight * rows) - (buttonSpacing * (rows-1)))) / alignment.y + marginY;
-
-        for (int i = 0; i < columns; i++)
-        {
-            for (int j = 0; j < rows; j++) {
-                if (ImGui.button("Button" + i + j, buttonWidth, buttonHeight))
-                {
-                    System.out.println("Button" + i + j + " pressed");
-                }
-                ImGui.sameLine();
-                currentY += buttonHeight + buttonSpacing;
-            }
-            currentX += buttonWidth + buttonSpacing;
-            currentY = (Window.getHeight() - ((buttonHeight * rows) - (buttonSpacing * (rows-1)))) / alignment.y + marginY;
-        }
-    }
-
-
-
 }
