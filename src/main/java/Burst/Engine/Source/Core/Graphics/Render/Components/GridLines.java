2edad6c7480ce324abbd4b38e48f6eccf1a5476c
DotNaos <schuetzoliver00@gmail.com>
2023-05-16 21:07:45 +0200

    Ist mir auch egal

--- src/main/java/Burst/Engine/Source/Core/Graphics/Render/Components/GridLines.java
+++ src/main/java/Burst/Engine/Source/Core/Graphics/Render/Components/GridLines.java
@@ -50,13 +50,16 @@ public class GridLines {
         height = (float) Math.floor(height);
 
 
-
         // Viewportpos is in the center of the viewport, so we need to offset it by half
         float firstX = viewportPos.x - zoom;
         float firstY = viewportPos.y - zoom / aspectRatio;
 
-        firstX = (float) Math.floor(firstX);
-        firstY = (float) Math.floor(firstY);
+        firstX = (float) Math.floor(firstX) - gridSize / 2;
+        firstY = (float) Math.floor(firstY) - gridSize / 2;
+        
+
+        int numLinesX = (int) (width / gridSize);
+        int numLinesY = (int) (height / gridSize);
 
 
         // DebugDraw.addBox2D(new Vector2f(), new Vector2f(1f, 1f), 0.0f, new Vector4f(0, 0, 1, 1));
@@ -64,14 +67,15 @@ public class GridLines {
         // Draw a Line from the viewport's position to the bottom left corner of the viewport
         // DebugDraw.addLine2D(viewportPos, new Vector2f(firstX, firstY), new Vector3f(0, 1, 0.1f));
 
-        // 
+
         System.out.println("ViewportPos: " + viewportPos.x + ", " + viewportPos.y + " | " + firstX + ", " + firstY + " | " + zoom + " | | | " + width + ", " + height);
 
 
-        for (int i = -100; i < 100; i++) {
-            DebugDraw.addLine2D(new Vector2f(i * gridSize, -100 * gridSize), new Vector2f(i * gridSize, 100 * gridSize), new Vector3f(0, 0, 0.1f));
-
-            
+        int numLines = Math.max(numLinesX, numLinesY);
+        Vector3f color = new Vector3f(0.2f, 0.2f, 0.2f);
+        for (int i = 0; i < numLines; i++) {
+            DebugDraw.addLine2D(new Vector2f(firstX + i * gridSize, firstY), new Vector2f(firstX + i * gridSize, firstY + height), color);
+            DebugDraw.addLine2D(new Vector2f(firstX, firstY + i * gridSize), new Vector2f(firstX + width, firstY + i * gridSize), color);
         }
 
 
