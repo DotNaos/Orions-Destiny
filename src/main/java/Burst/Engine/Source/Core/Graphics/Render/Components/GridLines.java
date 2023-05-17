444ab58583294be3885fab1a03e677254e672601
DotNaos <schuetzoliver00@gmail.com>
2023-05-17 20:34:04 +0200

    GridLines pixelSize

--- src/main/java/Burst/Engine/Source/Core/Graphics/Render/Components/GridLines.java
+++ src/main/java/Burst/Engine/Source/Core/Graphics/Render/Components/GridLines.java
@@ -4,7 +4,6 @@ import Burst.Engine.Config.Constants.GridLines_Config;
 import Burst.Engine.Source.Core.Graphics.Debug.DebugDraw;
 import Burst.Engine.Source.Core.UI.Viewport;
 import Burst.Engine.Source.Core.UI.Window;
-import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
@@ -20,6 +19,21 @@ import org.joml.Vector4f;
  *
  */
 public class GridLines {
+
+
+  /**
+   * The minimum Resolution of the grid lines in pixels.
+   *
+   * <p>
+   *   This is used to prevent the grid lines from becoming too small when zooming in.
+   *   The grid lines will be at least this many pixels wide and tall.
+   *   This value must be a power of 2.
+   *   This value must be greater than or equal to 16.
+   * </p>
+   *
+   */
+  public static int pixelsPerGrid = 1;
+
   /**
    * Updates the grid lines based on the current viewport's position, size, zoom
    * level, and projection size.
@@ -41,8 +55,9 @@ public class GridLines {
       float zoom = viewport.getZoom();
 
     // Scaling factor snaps to the nearest power of 2 of zoom
-      float scaling = (float) Math.pow(10, (int) Math.log10(zoom) - 1);
-      scaling = 1;
+
+      int nearestPowerOf2 = (int) Math.pow(2, Math.floor(Math.log(zoom) / Math.log(2) + pixelsPerGrid - 1));
+      float scaling = ((float) 1 / 16 * nearestPowerOf2);
 
       float gridSize = (GridLines_Config.SIZE * scaling);
 
@@ -51,24 +66,24 @@ public class GridLines {
       float width = viewport.getWorldSize().x;
       float height = viewport.getWorldSize().y;
 
-      width  = (float) Math.ceil(width);
-      height = (float) Math.ceil(height);
+      width  = (float) Math.ceil(width) + 3;
+      height = (float) Math.ceil(height) + 3;
 
 
     // Start to draw at this position
-      float firstX = viewport.getPosition().x - width / 2;
-      float firstY = viewport.getPosition().y - height / 2;
+      float firstX = viewport.getPosition().x - width / 2 - 1;
+      float firstY = viewport.getPosition().y - height / 2 - 1;
 
       // Align to the grid
-      firstX = (float) Math.ceil(firstX) - gridSize / 2;
-      firstY = (float) Math.ceil(firstY) - gridSize / 2;
+      firstX = (float) Math.floor(firstX) - gridSize / 2;
+      firstY = (float) Math.floor(firstY) - gridSize / 2;
 
     // Lines per axis, +2 for the lines that are at the edge
       int numLinesX = (int) (width / gridSize) + 2;
       int numLinesY = (int) (height / gridSize) + 2;
 
 
-    System.out.println("Pos: " + firstX + " " + firstY + " | " + width + " " + height);
+    System.out.println("Zoom: " + zoom + " | " + scaling + " | " + nearestPowerOf2);
 
     // Maximum number of lines
     int numLines = Math.max(numLinesX, numLinesY);
