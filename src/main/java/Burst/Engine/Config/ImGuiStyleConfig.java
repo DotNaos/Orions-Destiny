0761ee973c22b45f8d332757079b32aa15bee1f0
DotNaos <schuetzoliver00@gmail.com>
2023-05-18 19:32:38 +0200

    All Style Variables

--- src/main/java/Burst/Engine/Config/ImGuiStyleConfig.java
+++ src/main/java/Burst/Engine/Config/ImGuiStyleConfig.java
@@ -1,5 +1,7 @@
 package Burst.Engine.Config;
 
+import org.joml.Vector2f;
+
 import Burst.Engine.Source.Core.Component;
 import Burst.Engine.Source.Core.Util.Scalar;
 import imgui.ImGui;
@@ -12,6 +14,48 @@ public class ImGuiStyleConfig extends Component {
     private boolean dark = true;
     private static transient ImGuiStyleConfig instance = new ImGuiStyleConfig();
 
+    // All style
+    private float frameRounding = 0.0f;
+    private float childBorderSize = 1.0f;
+    private float alpha = 1.0f;
+    private boolean antiAliasedFill = true;
+    private boolean antiAliasedLines = true;
+    private boolean antiAliasedLinesUseTex = true;
+    private Vector2f buttonTextAlign = new Vector2f(0.5f, 0.5f);
+    private Vector2f cellPadding = new Vector2f(4.0f, 4.0f);
+    private float childRounding = 0.0f;
+    private float circleSegmentMaxError = 1.5f;
+    private int colorButtonPosition = 0;
+    private float columnsMinSpacing = 6.0f;
+    private float curveTessellationTol = 1.25f;
+    private Vector2f displaySafeAreaPadding = new Vector2f(3.0f, 3.0f);
+    private Vector2f displayWindowPadding = new Vector2f(4.0f, 4.0f);
+    private float frameBorderSize = 1.0f;
+    private Vector2f framePadding = new Vector2f(8.0f, 4.0f);
+    private float grabMinSize = 10.0f;
+    private float grabRounding = 0.0f;
+    private float indentSpacing = 21.0f;
+    private Vector2f itemInnerSpacing = new Vector2f(4.0f, 4.0f);
+    private Vector2f itemSpacing = new Vector2f(8.0f, 4.0f);
+    private float logSliderDeadzone = 4.0f;
+    private float mouseCursorScale = 1.0f;
+    private float popupBorderSize = 1.0f;
+    private float popupRounding = 0.0f;
+    private float scrollbarRounding = 0.0f;
+    private float scrollbarSize = 16.0f;
+    private Vector2f selectableTextAlign = new Vector2f(0.0f, 0.0f);
+    private float tabBorderSize = 0.0f;
+    private float tabMinWidthForCloseButton = 0.0f;
+    private float tabRounding = 0.0f;
+    private Vector2f touchExtraPadding = new Vector2f(0.0f, 0.0f);
+    private float windowBorderSize = 1.0f;
+    private int windowMenuButtonPosition = 1;
+    private Vector2f windowMinSize = new Vector2f(32.0f, 32.0f);
+    private Vector2f windowPadding = new Vector2f(8.0f, 8.0f);
+    private float windowRounding = 0.0f;
+    private Vector2f windowTitleAlign = new Vector2f(0.0f, 0.5f);
+
+
     @Override
     public void imgui()
     {
@@ -22,28 +66,46 @@ public class ImGuiStyleConfig extends Component {
         if (!active) return;
 
         ImGuiStyle style = ImGui.getStyle();
-        style.setFrameRounding(4);
-        style.setWindowRounding(10);
-        style.setScrollbarRounding(0);
-        style.setScrollbarSize(15);
-        style.setWindowBorderSize(0);
-        style.setChildBorderSize(0);
-        style.setPopupBorderSize(0);
-        style.setFrameBorderSize(0);
-        style.setTabBorderSize(1);
-        style.setTabRounding(2);
-        style.setWindowPadding(8, 8);
-        style.setFramePadding(4, 3);
-        style.setItemSpacing(8, 4);
-        style.setItemInnerSpacing(4, 4);
-        style.setIndentSpacing(21);
-        style.setScrollbarSize(15);
-        style.setGrabMinSize(5);
-        style.setWindowMenuButtonPosition(ImGuiDir.None);
-        style.setChildRounding(0);
-        style.setPopupRounding(0);
-        style.setScrollbarRounding(2);
-
+        style.setFrameRounding(this.frameRounding);
+        style.setChildBorderSize(this.childBorderSize);
+        style.setAlpha(this.alpha);
+        style.setAntiAliasedFill(this.antiAliasedFill);
+        style.setAntiAliasedLines (this.antiAliasedLines);
+        style.setAntiAliasedLinesUseTex (this.antiAliasedLinesUseTex);
+        style.setButtonTextAlign (this.buttonTextAlign.x, this.buttonTextAlign.y);
+        style.setCellPadding (this.cellPadding.x, this.cellPadding.y);
+        style.setChildBorderSize (this.childBorderSize);
+        style.setChildRounding (this.childRounding);
+        style.setCircleSegmentMaxError (this.circleSegmentMaxError);
+        style.setColorButtonPosition (this.colorButtonPosition);
+        style.setColumnsMinSpacing (this.columnsMinSpacing);
+        style.setCurveTessellationTol (this.curveTessellationTol);
+        style.setDisplaySafeAreaPadding (this.displaySafeAreaPadding.x, this.displaySafeAreaPadding.y);
+        style.setDisplayWindowPadding (this.displayWindowPadding.x, this.displayWindowPadding.y);
+        style.setFrameBorderSize (this.frameBorderSize);
+        style.setFramePadding (this.framePadding.x, this.framePadding.y);
+        style.setGrabMinSize (this.grabMinSize);
+        style.setGrabRounding (this.grabRounding);
+        style.setIndentSpacing (this.indentSpacing);
+        style.setItemInnerSpacing (this.itemInnerSpacing.x, this.itemInnerSpacing.y);
+        style.setItemSpacing (this.itemSpacing.x, this.itemSpacing.y);
+        style.setLogSliderDeadzone (this.logSliderDeadzone);
+        style.setMouseCursorScale (this.mouseCursorScale);
+        style.setPopupBorderSize (this.popupBorderSize);
+        style.setPopupRounding (this.popupRounding);
+        style.setScrollbarRounding (this.scrollbarRounding); 
+        style.setScrollbarSize (this.scrollbarSize); 
+        style.setSelectableTextAlign    (this.selectableTextAlign.x, this.selectableTextAlign.y);
+        style.setTabBorderSize (this.tabBorderSize);
+        style.setTabMinWidthForCloseButton (this.tabMinWidthForCloseButton);
+        style.setTabRounding (this.tabRounding);
+        style.setTouchExtraPadding (this.touchExtraPadding.x, this.touchExtraPadding.y);
+        style.setWindowBorderSize (this.windowBorderSize);
+        style.setWindowMenuButtonPosition (this.windowMenuButtonPosition);
+        style.setWindowMinSize (this.windowMinSize.x, this.windowMinSize.y);
+        style.setWindowPadding (this.windowPadding.x, this.windowPadding.y);
+        style.setWindowRounding (this.windowRounding);
+        style.setWindowTitleAlign (this.windowTitleAlign.x, this.windowTitleAlign.y);
 
 
         // Background
