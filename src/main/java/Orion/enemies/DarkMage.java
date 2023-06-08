5e5086891c1fd63aa67a78b9f04c10cd8a61a025
Dominik Abler <dominikabler33@gmail.com>
2023-06-08 21:05:54 +0200

    Introducing: enemies (warum machen wir unser projekt nicht so wie blizzard und brechen einfach ab?)

--- src/main/java/Orion/enemies/DarkMage.java
+++ src/main/java/Orion/enemies/DarkMage.java
@@ -1,8 +1,16 @@
 package Orion.enemies;
 
 public class DarkMage extends  EnemyCharacter{
+
     public DarkMage(){
         super("Evil Wizard");
+        this.HP = 200;
+        this.ATK = 10;
+        this.DEF = 10;
+        this.SPD = 10;
+        this.LVL = 1;
+
+
     }
 
 }
--- src/main/java/Orion/enemies/Ghost.java
+++ src/main/java/Orion/enemies/Ghost.java
@@ -3,5 +3,10 @@ package Orion.enemies;
 public class Ghost extends EnemyCharacter{
     public Ghost(){
         super("Ghost");
+        this.HP = 200;
+        this.ATK = 10;
+        this.DEF = 10;
+        this.SPD = 10;
+        this.LVL = 1;
     }
 }
